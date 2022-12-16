c76deef4a2b3b0086d04ff4fe7ca8742cd9af582
Luan Santos <github@luan.sh>
2022-12-16 11:04:35 -0800

    wow this maybe works

--- src/Scope.ts
+++ src/Scope.ts
@@ -1,4 +1,5 @@
 import { timeStamp } from "console";
+import { glob } from "glob";
 import * as path from "path";
 import * as vscode from "vscode";
 
@@ -20,6 +21,21 @@ const defaultScopes: JSONScopes = {
 };
 
 const CONFIG = "scopes";
+
+function intersect<T>(...sets: Set<T>[]) {
+  if (!sets.length) {
+    return new Set<T>();
+  }
+  const i = sets.reduce((m, s, i) => (s.size < sets[m].size ? i : m), 0);
+  const [smallest] = sets.splice(i, 1);
+  const res = new Set<T>();
+  for (let val of smallest) {
+    if (sets.every((s) => s.has(val))) {
+      res.add(val);
+    }
+  }
+  return res;
+}
 
 export class Scope {
   private scopes: Record<string, ScopeSettings> = {};
@@ -62,17 +78,14 @@ export class Scope {
     return this.scopes[this.activeScope];
   }
 
-  add(list: "included" | "excluded", val: string) {
-    const path = vscode.workspace.asRelativePath(val);
-    this.scope["included"].delete(path);
-    this.scope["excluded"].delete(path);
-    this.scope[list].add(path);
-    this.saveScopes();
-  }
-
-  remove(list: "included" | "excluded", val: string) {
-    const path = vscode.workspace.asRelativePath(val);
-    this.scope[list].delete(path);
+  toggleItem(list: "included" | "excluded", path: string) {
+    const other = list === "included" ? "excluded" : "included";
+    if (this.scope[list].has(path)) {
+      this.scope[list].delete(path);
+    } else {
+      this.scope[other].delete(path);
+      this.scope[list].add(path);
+    }
     this.saveScopes();
   }
 
@@ -104,24 +117,56 @@ export class Scope {
     });
   }
 
-  private generateExclusionGlobs(): Record<string, true> {
+  private async generateExclusionGlobs(): Promise<Record<string, true>> {
     let result: Record<string, true> = { ...this.globalExclude };
     if (!this.enabled) {
       return result;
     }
+    const rel = vscode.workspace.asRelativePath;
 
-    this.scope.excluded.forEach((name: string) => {
-      result[name] = true;
+    this.scope.excluded.forEach((path) => {
+      result[rel(path)] = true;
     });
+
+    let sets: Set<string>[] = [];
+    for (const folderPath of this.scope.included) {
+      const uri = vscode.Uri.parse(folderPath);
+      const root = vscode.workspace.getWorkspaceFolder(uri);
+      if (!root) {
+        continue;
+      }
+      const set = new Set<string>();
+      const rootPath = root.uri.fsPath;
+      let folder = folderPath;
+      let parent = path.dirname(folder);
+      while (parent.length >= rootPath.length) {
+        const siblings = glob.sync(path.join(parent, "*"), {
+          ignore: folder,
+          dot: true,
+        });
+        siblings.forEach((p) => set.add(p));
+        folder = parent;
+        parent = path.dirname(parent);
+      }
+
+      set.delete(path.join(rootPath, "/"));
+      sets.push(set);
+    }
+
+    const exclusionsFromInclusions = intersect(...sets);
+    exclusionsFromInclusions.forEach((path) => {
+      result[rel(path)] = true;
+    });
+
     return result;
   }
 
-  private updateFilesExclude() {
+  private async updateFilesExclude() {
     vscode.workspace
       .getConfiguration()
       .update(
         "files.exclude",
-        this.generateExclusionGlobs(),
+        await this.generateExclusionGlobs(),
         vscode.ConfigurationTarget.Global
       );
   }
--- src/extension.ts
+++ src/extension.ts
@@ -18,17 +18,11 @@ export function activate(context: vscode.ExtensionContext) {
       vscode.commands.registerCommand("scopes.toggle", (args) =>
         scope.toggle()
       ),
-      vscode.commands.registerCommand("scopes.addInclusion", (args) =>
-        scope.add("included", args.path)
+      vscode.commands.registerCommand("scopes.toggleInclusion", (args) =>
+        scope.toggleItem("included", args.path)
       ),
-      vscode.commands.registerCommand("scopes.removeInclusion", (args) =>
-        scope.remove("included", args.path)
-      ),
-      vscode.commands.registerCommand("scopes.addExclusion", (args) =>
-        scope.add("excluded", args.path)
-      ),
-      vscode.commands.registerCommand("scopes.removeExclusion", (args) =>
-        scope.remove("excluded", args.path)
+      vscode.commands.registerCommand("scopes.toggleExclusion", (args) =>
+        scope.toggleItem("excluded", args.path)
       ),
     ]
   );
